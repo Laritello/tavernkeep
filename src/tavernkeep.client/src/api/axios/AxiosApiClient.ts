7f5f64928572382b0e2c050ea658027d888e4f59
Alex Putin <puellamagi999@gmail.com>
2024-02-16 19:56:07 +0300

    Base url for api and signalr hub

--- src/tavernkeep.client/src/api/axios/AxiosApiClient.ts
+++ src/tavernkeep.client/src/api/axios/AxiosApiClient.ts
@@ -1,95 +1,145 @@
-import axios, { type AxiosInstance } from 'axios';
-import type { ApiClient } from '../base/ApiClient';
-import type { ApiResponse } from '../base/ApiResponse';
-import { AxiosApiResponse } from './AxiosApiResponse';
-import { User } from '@/entities/User';
-import { UserRole } from '@/contracts/enums/UserRole';
-import { getCookie } from 'typescript-cookie'
-import { Message } from '@/entities/Message';
-import { MessageType } from '@/contracts/enums/MessageType';
-import { Character } from '@/entities/Character';
+import axios, { type AxiosInstance } from "axios";
+import type { ApiClient } from "../base/ApiClient";
+import type { ApiResponse } from "../base/ApiResponse";
+import { AxiosApiResponse } from "./AxiosApiResponse";
+import { User } from "@/entities/User";
+import { UserRole } from "@/contracts/enums/UserRole";
+import { getCookie } from "typescript-cookie";
+import { Message } from "@/entities/Message";
+import { MessageType } from "@/contracts/enums/MessageType";
+import { Character } from "@/entities/Character";
 
 // TODO: Error handling and interceptors
 // TODO: Decorators might be usefull here as I do similar logic every time.
 // TODO: Move cookie name somewhere where it will be set globally.
 export class AxiosApiClient implements ApiClient {
-    client: AxiosInstance;
-    private baseURL = 'https://localhost:7231/api/';
-    private cookieName: string = 'taverkeep.auth.jwt';
+  client: AxiosInstance;
+  private baseURL = "https://" + window.location.hostname + ":7231/api/";
+  private cookieName: string = "taverkeep.auth.jwt";
 
-    constructor() {
-        this.client = axios.create({
-            baseURL: this.baseURL,
-            timeout: 5000, // 5 seconds
-            headers: {
-                "Content-Type": "application/json",
-            }
-        });
-    }
+  constructor() {
+    this.client = axios.create({
+      baseURL: this.baseURL,
+      timeout: 5000, // 5 seconds
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+  }
 
-    async auth(login: string, password: string): Promise<ApiResponse<string>> {
-        const response = await this.client.post<string>('authentication/auth', {
-            login: login,
-            password: password
-        });
+  async auth(login: string, password: string): Promise<ApiResponse<string>> {
+    const response = await this.client.post<string>("authentication/auth", {
+      login: login,
+      password: password,
+    });
 
-        return new AxiosApiResponse(response.data, response.status, response.statusText);
-    }
+    return new AxiosApiResponse(
+      response.data,
+      response.status,
+      response.statusText
+    );
+  }
 
-    async getUsers(): Promise<ApiResponse<User[]>> {
-        const response = await this.client.get<User[]>('users');
-        return new AxiosApiResponse(response.data, response.status, response.statusText);
-    }
+  async getUsers(): Promise<ApiResponse<User[]>> {
+    const response = await this.client.get<User[]>("users");
+    return new AxiosApiResponse(
+      response.data,
+      response.status,
+      response.statusText
+    );
+  }
 
-    // TODO: User request types instead of separate parameters
-    async createUser(login: string, password: string, role: UserRole): Promise<ApiResponse<User>> {
-        const response = await this.client.post<User>('users/create', {
-            login: login,
-            password: password,
-            role: role
-        }, { headers: { "Authorization": "Bearer " + getCookie(this.cookieName) } });
+  // TODO: User request types instead of separate parameters
+  async createUser(
+    login: string,
+    password: string,
+    role: UserRole
+  ): Promise<ApiResponse<User>> {
+    const response = await this.client.post<User>(
+      "users/create",
+      {
+        login: login,
+        password: password,
+        role: role,
+      },
+      { headers: { Authorization: "Bearer " + getCookie(this.cookieName) } }
+    );
 
-        return new AxiosApiResponse(response.data, response.status, response.statusText)
-    }
+    return new AxiosApiResponse(
+      response.data,
+      response.status,
+      response.statusText
+    );
+  }
 
-    // TODO: ApiResponse for empty responses
-    async deleteUser(id: number): Promise<ApiResponse<null>> {
-        const response = await this.client.delete('users/delete/' + id,
-            { headers: { "Authorization": "Bearer " + getCookie(this.cookieName) } });
+  // TODO: ApiResponse for empty responses
+  async deleteUser(id: number): Promise<ApiResponse<null>> {
+    const response = await this.client.delete("users/delete/" + id, {
+      headers: { Authorization: "Bearer " + getCookie(this.cookieName) },
+    });
 
-        return new AxiosApiResponse(null, response.status, response.statusText);
-    }
+    return new AxiosApiResponse(null, response.status, response.statusText);
+  }
 
-    async createCharacter(name: string): Promise<ApiResponse<Character>> {
-        const response = await this.client.post<Character>('characters/create',
-            { name: name },
-            { headers: { "Authorization": "Bearer " + getCookie(this.cookieName) } });
+  async createCharacter(name: string): Promise<ApiResponse<Character>> {
+    const response = await this.client.post<Character>(
+      "characters/create",
+      { name: name },
+      { headers: { Authorization: "Bearer " + getCookie(this.cookieName) } }
+    );
 
-        return new AxiosApiResponse(response.data, response.status, response.statusText);
-    }
+    return new AxiosApiResponse(
+      response.data,
+      response.status,
+      response.statusText
+    );
+  }
 
-    async getCharacter(id: string): Promise<ApiResponse<Character>> {
-        const response = await this.client.get<Character>('characters/' + id,
-            { headers: { "Authorization": "Bearer " + getCookie(this.cookieName) } })
+  async getCharacter(id: string): Promise<ApiResponse<Character>> {
+    const response = await this.client.get<Character>("characters/" + id, {
+      headers: { Authorization: "Bearer " + getCookie(this.cookieName) },
+    });
 
-        return new AxiosApiResponse(response.data, response.status, response.statusText);
-    }
+    return new AxiosApiResponse(
+      response.data,
+      response.status,
+      response.statusText
+    );
+  }
 
-    async sendMessage(content: string, type: MessageType): Promise<ApiResponse<Message>> {
-        const response = await this.client.post<Message>('chat', {
-            type: type,
-            content: content
-        }, { headers: { "Authorization": "Bearer " + getCookie(this.cookieName) } })
+  async sendMessage(
+    content: string,
+    type: MessageType
+  ): Promise<ApiResponse<Message>> {
+    const response = await this.client.post<Message>(
+      "chat",
+      {
+        type: type,
+        content: content,
+      },
+      { headers: { Authorization: "Bearer " + getCookie(this.cookieName) } }
+    );
 
-        return new AxiosApiResponse(response.data, response.status, response.statusText)
-    }
+    return new AxiosApiResponse(
+      response.data,
+      response.status,
+      response.statusText
+    );
+  }
 
-    async getMessages(skip: number, take: number): Promise<ApiResponse<Message[]>> {
-        const response = await this.client.get<Message[]>('chat', {
-            params: { skip: skip, take: take },
-            headers: { "Authorization": "Bearer " + getCookie(this.cookieName) }
-        })
+  async getMessages(
+    skip: number,
+    take: number
+  ): Promise<ApiResponse<Message[]>> {
+    const response = await this.client.get<Message[]>("chat", {
+      params: { skip: skip, take: take },
+      headers: { Authorization: "Bearer " + getCookie(this.cookieName) },
+    });
 
-        return new AxiosApiResponse(response.data, response.status, response.statusText)
-    }
+    return new AxiosApiResponse(
+      response.data,
+      response.status,
+      response.statusText
+    );
+  }
 }
--- src/tavernkeep.client/src/api/hubs/ChatHub.ts
+++ src/tavernkeep.client/src/api/hubs/ChatHub.ts
@@ -1,23 +1,27 @@
-import { HttpTransportType, HubConnection, HubConnectionBuilder } from "@microsoft/signalr"
+import {
+  HttpTransportType,
+  HubConnection,
+  HubConnectionBuilder,
+} from "@microsoft/signalr";
 
 class ChatHub {
-    private baseURL = 'https://localhost:7231/api/';
+  private baseURL = "https://" + window.location.hostname + ":7231/api/";
 
-    connection: HubConnection
+  connection: HubConnection;
 
-    constructor() {
-        this.connection = new HubConnectionBuilder()
-            .withUrl(this.baseURL + "hubs/chat", {
-                skipNegotiation: true,
-                transport: HttpTransportType.WebSockets
-            })
-            .build();
-        this.start()
-    }
+  constructor() {
+    this.connection = new HubConnectionBuilder()
+      .withUrl(this.baseURL + "hubs/chat", {
+        skipNegotiation: true,
+        transport: HttpTransportType.WebSockets,
+      })
+      .build();
+    this.start();
+  }
 
-    start() {
-        this.connection.start();
-    }
+  start() {
+    this.connection.start();
+  }
 }
 
 // Singleton
