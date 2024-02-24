fa26bf735a058edd06677580d0e344243f4cdcd2
Alex Putin <puellamagi999@gmail.com>
2024-02-24 20:59:23 +0300

    fetch characters on main page

--- src/tavernkeep.client/src/pages/HomePage.vue
+++ src/tavernkeep.client/src/pages/HomePage.vue
@@ -4,4 +4,9 @@
 
 <script setup lang="ts">
 import PartyComponent from '@/components/PartyComponent.vue';
+
+import { useCharactersStore } from '@/stores/characters.store';
+const characterStore = useCharactersStore();
+
+characterStore.fetchCharacters();
 </script>
