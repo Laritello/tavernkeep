9b7301d56b81510b9c30c202e571ee4c3366c3a4
Alex Putin <puellamagi999@gmail.com>
2024-02-26 17:32:11 +0300

    add character creation to admin panel

--- src/tavernkeep.client/src/pages/AdminPanelPage.vue
+++ src/tavernkeep.client/src/pages/AdminPanelPage.vue
@@ -8,11 +8,27 @@
             <v-card>
                 <UserList />
             </v-card>
+            <v-card>
+                <v-sheet class="mx-auto">
+                    <v-form fast-fail @submit.prevent="charactersStore.createCharacter(characterName)" class="pa-2">
+                        <div class="text-h6">Create character</div>
+                        <v-text-field v-model="characterName" label="Login"></v-text-field>
+                        <v-btn type="submit" block>Create</v-btn>
+                    </v-form>
+                </v-sheet>
+            </v-card>
         </div>
     </div>
 </template>
 
 <script setup lang="ts">
+import { ref } from 'vue';
+
+import { useCharactersStore } from '@/stores/characters.store';
+
 import UserForm from '@/components/UserForm.vue';
 import UserList from '@/components/UserList.vue';
+
+const charactersStore = useCharactersStore();
+const characterName = ref('');
 </script>
