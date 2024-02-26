16ce099dd6200c5fc48b15fac7c6c6c332fb0769
Alex Putin <puellamagi999@gmail.com>
2024-02-26 15:04:16 +0300

    chat hub subscription moved into message store

--- src/tavernkeep.client/src/stores/messages.store.ts
+++ src/tavernkeep.client/src/stores/messages.store.ts
@@ -2,12 +2,17 @@ import { ref } from 'vue';
 import { defineStore } from 'pinia';
 import { plainToInstance } from 'class-transformer';
 
+import ChatHub from '@/api/hubs/ChatHub';
 import { ApiClientFactory } from '@/factories/ApiClientFactory';
 import type { ApiClient } from '@/api/base/ApiClient';
 import { Message, RollMessage, TextMessage } from '@/entities/Message';
 
-const api: ApiClient = ApiClientFactory.createApiClient();
 export const useMessagesStore = defineStore('messages.store', () => {
+    const api: ApiClient = ApiClientFactory.createApiClient();
+    ChatHub.connection.on('ReceiveMessage', (msg: Message) => {
+        appendMessage(msg);
+    });
+
     const messages = ref<Message[]>([]);
 
     async function fetchMessages(skip: number, take: number) {
@@ -39,5 +44,5 @@ export const useMessagesStore = defineStore('messages.store', () => {
         messages.value.push(typedMessage);
     }
 
-    return { messages, fetchMessages, createMessage, appendMessage };
+    return { messages, fetchMessages, createMessage };
 });
