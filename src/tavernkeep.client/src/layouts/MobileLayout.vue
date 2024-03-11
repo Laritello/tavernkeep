30a56bf186a59114528b8d390c1c0fc522b1ec4e
Alex Putin <puellamagi999@gmail.com>
2024-03-11 15:39:38 +0300

    add mobile layout

---
+++ src/tavernkeep.client/src/layouts/MobileLayout.vue
@@ -0,0 +1,57 @@
+<template>
+    <div class="flex flex-col h-screen">
+        <!-- Navbar -->
+        <div class="relative navbar bg-primary text-primary-content z-[1] shadow-sm shadow-neutral-950">
+            <div class="flex-1">
+                <h1 class="text-xl px-4 cursor-default">Tavernkeep</h1>
+            </div>
+            <label class="flex cursor-pointer gap-2 mr-4">
+                <span class="label-text text-primary-content">Light</span>
+                <input type="checkbox" value="dark" class="toggle theme-controller" />
+                <span class="label-text text-primary-content">Dark</span>
+            </label>
+            <div class="flex-none text-base-content">
+                <span class="text-primary-content">{{ appStore.users.current?.login }}</span>
+                <div class="dropdown dropdown-end">
+                    <div tabindex="0" role="button" class="btn btn-ghost btn-circle avatar">
+                        <div class="avatar w-10 rounded-full bg-orange-400"></div>
+                    </div>
+                    <ul
+                        tabindex="0"
+                        class="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-md w-52"
+                    >
+                        <li>
+                            <a @click.prevent="logout">Logout</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        <div class="flex flex-row content-height">
+            <slot />
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+import { useRouter } from 'vue-router';
+
+import { useAppStore } from '@/stores/app.store';
+
+const appStore = useAppStore();
+const router = useRouter();
+
+async function logout() {
+    appStore.auth.logout();
+    router.push('/login');
+}
+</script>
+<style scoped>
+.content-height {
+    height: calc(100% - 4rem);
+}
+
+.router-link-active {
+    @apply active;
+}
+</style>
--- src/tavernkeep.client/src/layouts/index.ts
+++ src/tavernkeep.client/src/layouts/index.ts
@@ -1,4 +1,5 @@
 import BlankLayout from './BlankLayout.vue';
 import AppLayout from './AppLayout.vue';
+import MobileLayout from './MobileLayout.vue';
 
-export default { BlankLayout, AppLayout };
+export default { BlankLayout, AppLayout, MobileLayout };
