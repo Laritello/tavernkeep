71d150997c5428339a8c1e227ee1391d2020e720
Alex Putin <puellamagi999@gmail.com>
2024-05-28 16:47:09 +0300

    loading transition

--- src/tavernkeep.client/src/main.ts
+++ src/tavernkeep.client/src/main.ts
@@ -1,7 +1,7 @@
 import './assets/main.css';
 import '@mdi/font/css/materialdesignicons.css';
 
-import { createApp, watch } from 'vue';
+import { createApp } from 'vue';
 import App from './App.vue';
 
 import { createPinia } from 'pinia';
@@ -16,13 +16,7 @@ import * as directives from 'vuetify/directives';
 
 // Router
 import { router } from './router';
-import ChatHub from './api/hubs/ChatHub';
-import CharacterHub from './api/hubs/CharacterHub';
-
-import { useSession } from './composables/useSession';
-import { useUsers } from './stores/users';
-import { useCharacters } from './stores/characters';
-import { useMessages } from './stores/messages';
+import { useAppState } from './stores/appState';
 
 const vuetify = createVuetify({
     components,
@@ -39,47 +33,6 @@ app.use(pinia);
 app.use(router);
 app.use(VueChatScroll);
 
-const session = useSession();
-const users = useUsers();
-const characters = useCharacters();
-const messages = useMessages();
-
-console.groupCollapsed('[AppStore] Initialize');
-if (session.isAuthenticated.value) {
-    console.info('[AppStore] User already logged in. Fetching data...');
-    initialize().then(() => {
-        console.info('[AppStore] Initialized');
-        console.groupEnd();
-    });
-}
-
-watch(session.isAuthenticated, (value): void => {
-    console.info('[AppStore] Auth status update');
-    if (!value) {
-        console.info('[AppStore] User logged out');
-        stopHubs().then(() => console.info('[AppStore] Hubs stopped'));
-        return;
-    }
-    console.info('[AppStore] User logged in. Fetching data...');
-    initialize().then(() => console.info('[AppStore] reInitialized'));
-});
-
-async function fetch(): Promise<void[]> {
-    return Promise.all([users.fetch(), characters.fetch(), messages.fetch(0, 20)]);
-}
-
-async function initialize() {
-    const fetchPromise = fetch().then(() => console.info('[AppStore] Initial data fetched'));
-    const startHubsPromise = startHubs().then(() => console.info('[AppStore] Hubs started'));
-    return Promise.all([fetchPromise, startHubsPromise]);
-}
-
-async function startHubs() {
-    return Promise.all([ChatHub.start(), CharacterHub.start()]);
-}
-
-async function stopHubs() {
-    return Promise.all([ChatHub.stop(), CharacterHub.stop()]);
-}
+useAppState();
 
 app.mount('#app');
---
+++ src/tavernkeep.client/src/stores/appState.ts
@@ -0,0 +1,58 @@
+import { defineStore } from 'pinia';
+import ChatHub from '@/api/hubs/ChatHub';
+import CharacterHub from '@/api/hubs/CharacterHub';
+
+import { useSession } from '@/composables/useSession';
+import { useUsers } from '@/stores/users';
+import { useCharacters } from '@/stores/characters';
+import { useMessages } from '@/stores/messages';
+import { ref, watch } from 'vue';
+
+export const useAppState = defineStore('appState', () => {
+    const session = useSession();
+    const users = useUsers();
+    const characters = useCharacters();
+    const messages = useMessages();
+    const isLoading = ref(true);
+
+    console.groupCollapsed('[AppStore] Initialize');
+    if (session.isAuthenticated.value) {
+        console.info('[AppStore] User already logged in. Fetching data...');
+        initialize().then(() => {
+            console.info('[AppStore] Initialized');
+            console.groupEnd();
+        });
+    }
+
+    watch(session.isAuthenticated, (value): void => {
+        console.info('[AppStore] Auth status update');
+        if (!value) {
+            console.info('[AppStore] User logged out');
+            stopHubs().then(() => console.info('[AppStore] Hubs stopped'));
+            return;
+        }
+        console.info('[AppStore] User logged in. Fetching data...');
+        initialize().then(() => console.info('[AppStore] reInitialized'));
+    });
+
+    async function fetch(): Promise<void[]> {
+        return Promise.all([users.fetch(), characters.fetch(), messages.fetch(0, 20)]);
+    }
+
+    async function initialize() {
+        const fetchPromise = fetch().then(() => console.info('[AppStore] Initial data fetched'));
+        const startHubsPromise = startHubs().then(() => console.info('[AppStore] Hubs started'));
+        await Promise.all([fetchPromise, startHubsPromise]);
+        isLoading.value = false;
+    }
+
+    async function startHubs() {
+        return Promise.all([ChatHub.start(), CharacterHub.start()]);
+    }
+
+    async function stopHubs() {
+        return Promise.all([ChatHub.stop(), CharacterHub.stop()]);
+    }
+
+    return { initialize, isLoading };
+});
