972d9cd072560549e52ee2871a78b580b708f8d3
Laritello <larik97@gmail.com>
2024-08-11 20:04:37 +0300

    Updated tests
    - EditPerceptionCommandTests: new

---
+++ tests/Tavernkepp.Application.Tests/UseCases/Characters/Commands/EditPerceptionCommandTests.cs
@@ -0,0 +1,148 @@
+﻿using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tavernkeep.Application.Interfaces;
+using Tavernkeep.Application.UseCases.Characters.Commands.EditSkill;
+using Tavernkeep.Core.Contracts.Enums;
+using Tavernkeep.Core.Entities.Pathfinder;
+using Tavernkeep.Core.Entities;
+using Tavernkeep.Core.Exceptions;
+using Tavernkeep.Core.Repositories;
+using Tavernkeep.Core.Specifications;
+using Tavernkeep.Application.UseCases.Characters.Commands.EditPerception;
+
+namespace Tavernkepp.Application.Tests.UseCases.Characters.Commands
+{
+	public class EditPerceptionCommandTests
+	{
+		private readonly Guid characterId = Guid.NewGuid();
+		private readonly Proficiency proficiency = Proficiency.Trained;
+
+		private readonly User owner;
+		private readonly User master;
+
+		private Character character;
+
+		public EditPerceptionCommandTests()
+		{
+			owner = new User("owner", "owner", UserRole.Player) { Id = Guid.NewGuid() };
+			master = new User("master", "master", UserRole.Master) { Id = Guid.NewGuid() };
+		}
+
+		[SetUp]
+		public void SetUp()
+		{
+			character = new Character()
+			{
+				Id = characterId,
+				Name = "Demo",
+				Owner = owner
+			};
+		}
+
+		[Test]
+		public async Task EditPerceptionCommand_Success()
+		{
+			var mockUserRepository = new Mock<IUserRepository>();
+			var mockCharacterRepository = new Mock<ICharacterRepository>();
+			var mockNotificationService = new Mock<INotificationService>();
+
+			mockUserRepository
+				.Setup(repo => repo.FindAsync(owner.Id, It.IsAny<ISpecification<User>>(), It.IsAny<CancellationToken>()))
+				.ReturnsAsync(owner);
+			mockCharacterRepository
+				.Setup(repo => repo.GetFullCharacterAsync(characterId, It.IsAny<CancellationToken>()))
+				.ReturnsAsync(character);
+
+			var request = new EditPerceptionCommand(owner.Id, characterId, proficiency);
+			var handler = new EditPerceptionCommandHandler(mockUserRepository.Object, mockCharacterRepository.Object, mockNotificationService.Object);
+
+			var response = await handler.Handle(request, CancellationToken.None);
+
+			Assert.That(response.Proficiency, Is.EqualTo(proficiency));
+		}
+
+		[Test]
+		public async Task EditPerceptionCommand_Success_Master()
+		{
+			var mockUserRepository = new Mock<IUserRepository>();
+			var mockCharacterRepository = new Mock<ICharacterRepository>();
+			var mockNotificationService = new Mock<INotificationService>();
+
+			mockUserRepository
+				.Setup(repo => repo.FindAsync(master.Id, It.IsAny<ISpecification<User>>(), It.IsAny<CancellationToken>()))
+				.ReturnsAsync(master);
+			mockCharacterRepository
+				.Setup(repo => repo.GetFullCharacterAsync(characterId, It.IsAny<CancellationToken>()))
+				.ReturnsAsync(character);
+
+			var request = new EditPerceptionCommand(master.Id, characterId, proficiency);
+			var handler = new EditPerceptionCommandHandler(mockUserRepository.Object, mockCharacterRepository.Object, mockNotificationService.Object);
+
+			var response = await handler.Handle(request, CancellationToken.None);
+
+			Assert.That(response.Proficiency, Is.EqualTo(proficiency));
+		}
+
+		[Test]
+		public void EditPerceptionCommand_InitiatorNotFound()
+		{
+			var mockUserRepository = new Mock<IUserRepository>();
+			var mockCharacterRepository = new Mock<ICharacterRepository>();
+			var mockNotificationService = new Mock<INotificationService>();
+
+			mockCharacterRepository
+				.Setup(repo => repo.GetFullCharacterAsync(characterId, It.IsAny<CancellationToken>()))
+				.ReturnsAsync(character);
+
+			var request = new EditPerceptionCommand(owner.Id, characterId, proficiency);
+			var handler = new EditPerceptionCommandHandler(mockUserRepository.Object, mockCharacterRepository.Object, mockNotificationService.Object);
+
+			var ex = Assert.ThrowsAsync<BusinessLogicException>(async () => await handler.Handle(request, CancellationToken.None));
+			Assert.That(ex.Message, Is.EqualTo("User with specified ID doesn't exist."));
+		}
+
+		[Test]
+		public void EditPerceptionCommand_CharacterNotFound()
+		{
+			var mockUserRepository = new Mock<IUserRepository>();
+			var mockCharacterRepository = new Mock<ICharacterRepository>();
+			var mockNotificationService = new Mock<INotificationService>();
+
+			mockUserRepository
+				.Setup(repo => repo.FindAsync(owner.Id, It.IsAny<ISpecification<User>>(), It.IsAny<CancellationToken>()))
+				.ReturnsAsync(owner);
+
+			var request = new EditPerceptionCommand(owner.Id, characterId, proficiency);
+			var handler = new EditPerceptionCommandHandler(mockUserRepository.Object, mockCharacterRepository.Object, mockNotificationService.Object);
+
+			var ex = Assert.ThrowsAsync<BusinessLogicException>(async () => await handler.Handle(request, CancellationToken.None));
+			Assert.That(ex.Message, Is.EqualTo("Character with specified ID doesn't exist."));
+		}
+
+		[Test]
+		public void EditPerceptionCommand_NotEnoughPermissions()
+		{
+			var mockUserRepository = new Mock<IUserRepository>();
+			var mockCharacterRepository = new Mock<ICharacterRepository>();
+			var mockNotificationService = new Mock<INotificationService>();
+			var initiatorId = Guid.NewGuid();
+
+			mockUserRepository
+				.Setup(repo => repo.FindAsync(initiatorId, It.IsAny<ISpecification<User>>(), It.IsAny<CancellationToken>()))
+				.ReturnsAsync(new User(string.Empty, string.Empty, UserRole.Player));
+			mockCharacterRepository
+				.Setup(repo => repo.GetFullCharacterAsync(characterId, It.IsAny<CancellationToken>()))
+				.ReturnsAsync(character);
+
+			var request = new EditPerceptionCommand(initiatorId, characterId, proficiency);
+			var handler = new EditPerceptionCommandHandler(mockUserRepository.Object, mockCharacterRepository.Object, mockNotificationService.Object);
+
+			var ex = Assert.ThrowsAsync<InsufficientPermissionException>(async () => await handler.Handle(request, CancellationToken.None));
+			Assert.That(ex.Message, Is.EqualTo("You do not have the necessary permissions to perform this operation."));
+		}
+	}
+}
