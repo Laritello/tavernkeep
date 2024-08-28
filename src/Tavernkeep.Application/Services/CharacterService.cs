90f29882284b10dc841d9aa6399aeccecfd782ce
Laritello <larik97@gmail.com>
2024-08-29 00:35:53 +0300

    Update CharacterService.cs

--- src/Tavernkeep.Application/Services/CharacterService.cs
+++ src/Tavernkeep.Application/Services/CharacterService.cs
@@ -1,12 +1,17 @@
 ﻿using Tavernkeep.Application.Interfaces;
 using Tavernkeep.Core.Entities;
 using Tavernkeep.Core.Entities.Pathfinder;
+using Tavernkeep.Core.Entities.Pathfinder.Builds.Conversion.Converters;
 using Tavernkeep.Core.Exceptions;
 using Tavernkeep.Core.Repositories;
 
 namespace Tavernkeep.Application.Services
 {
-	public class CharacterService(ICharacterRepository characterRepository) : ICharacterService
+	public class CharacterService(
+		ICharacterRepository characterRepository,
+		IAncestryTemplateRepository ancestryRepository,
+		IClassTemplateRepository classRepository
+		) : ICharacterService
 	{
 		public async Task<Character> CreateCharacterAsync(User owner, string name, CancellationToken cancellationToken = default)
 		{
@@ -26,6 +31,20 @@ namespace Tavernkeep.Application.Services
 		{
 			var character = await characterRepository.FindAsync(id, cancellationToken: cancellationToken)
 				?? throw new BusinessLogicException("No character with provided ID found.");
+
+			var ancestryTemplate = await ancestryRepository.FindAsync(character.Snapshot.Ancestry.Id, cancellationToken: cancellationToken)
+				?? throw new BusinessLogicException("No ancestry with provided ID found.");
+
+			var classTemplate = await classRepository.FindAsync(character.Snapshot.Class.Id, cancellationToken: cancellationToken)
+				?? throw new BusinessLogicException("No class with provided ID found.");
+
+			var generalConverter = new GeneralConverter(character);
+			var ancestryConverter = new AncestryConverter(character, ancestryTemplate);
+			var classConverter = new ClassConverter(character, ancestryTemplate, classTemplate);
+
+			character.Build.General = generalConverter.Convert();
+			character.Build.Ancestry = ancestryConverter.Convert();
+			character.Build.Class = classConverter.Convert();
 
 			return character;
 		}
