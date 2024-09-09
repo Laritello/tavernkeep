80c5e6e271c04e12dd7eff6f2a73bf6868a31fd2
Alex Putin <puellamagi999@gmail.com>
2024-09-09 14:59:19 +0300

    Remove unused layout type

--- src/tavernkeep.client/src/types.d.ts
+++ src/tavernkeep.client/src/types.d.ts
@@ -6,7 +6,7 @@ declare module 'vue-router' {
         protected: boolean;
         allowedRoles?: UserRole[];
         errorMessage?: string;
-        layout?: 'AppLayout' | 'BlankLayout' | 'MobileLayout';
+        layout?: 'AppLayout' | 'BlankLayout';
     }
 }
 
