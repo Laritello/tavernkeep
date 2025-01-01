d45d270af21aca1b0b40a8a5946999a6d82c73ac
Laritello <larik97@gmail.com>
2025-01-01 15:20:21 +0300

    Top header refactoring

---
+++ src/tavernkeep.client/src/components/TopNavigationView.vue
@@ -0,0 +1,92 @@
+<script setup lang="ts">
+import { useRouter } from 'vue-router';
+
+import { useAuth } from '@/composables/useAuth';
+import { useSession } from '@/composables/useSession';
+import { useModal } from '@/composables/useModal';
+import ConfirmationDialog from '@/components/dialogs/ConfirmationDialog.vue';
+import HealthBar from '@/components/character/HealthBar.vue';
+
+import { UserRole } from '@/contracts/enums';
+import { useCurrentUserAccount } from '@/composables/useCurrentUserAccount';
+import ArmorClassWidget from '@/components/character/ArmorClassWidget.vue';
+import PerceptionWidget from '@/components/character/PerceptionWidget.vue';
+
+const session = useSession();
+const router = useRouter();
+const user = useCurrentUserAccount();
+
+async function logout() {
+    const modal = useModal();
+    const modalResult = await modal.show(ConfirmationDialog, {
+        caption: 'Logout',
+        message: 'You sure you want to logout?',
+    });
+
+    if (modalResult.action !== 'confirm') return;
+    const auth = useAuth();
+    auth.logout();
+    await router.push('/login');
+}
+</script>
+
+<template>
+    <div class="navbar bg-base-100 z-10">
+        <div class="flex-none gap-2">
+            <div class="dropdown">
+                <div tabindex="0" role="button" class="btn btn-ghost btn-circle avatar">
+                    <div class="w-10 rounded-full">
+                        <img alt="Tailwind CSS Navbar component"
+                            src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
+                    </div>
+                </div>
+                <ul tabindex="0"
+                    class="menu menu-sm dropdown-content bg-base-300 rounded-box z-[1] mt-3 w-52 p-2 shadow-lg">
+                    <li>
+                        <a class="justify-between" @click="$router.push('/characters')">
+                            {{ session.userLogin.value }}
+                            <span class="badge font-bold">Characters</span>
+                        </a>
+                    </li>
+                    <li v-if="session.userRole.value === UserRole.Master">
+                        <a @click="$router.push('/admin')">GM panel</a>
+                    </li>
+                    <li>
+                        <label class="swap swap-rotate">
+                            Toggle theme
+                            <!-- this hidden checkbox controls the state -->
+                            <input type="checkbox" class="theme-controller" value="dark" />
+
+                            <!-- sun icon -->
+                            <svg class="swap-off h-10 w-10 fill-current" xmlns="http://www.w3.org/2000/svg"
+                                viewBox="0 0 24 24">
+                                <path
+                                    d="M5.64,17l-.71.71a1,1,0,0,0,0,1.41,1,1,0,0,0,1.41,0l.71-.71A1,1,0,0,0,5.64,17ZM5,12a1,1,0,0,0-1-1H3a1,1,0,0,0,0,2H4A1,1,0,0,0,5,12Zm7-7a1,1,0,0,0,1-1V3a1,1,0,0,0-2,0V4A1,1,0,0,0,12,5ZM5.64,7.05a1,1,0,0,0,.7.29,1,1,0,0,0,.71-.29,1,1,0,0,0,0-1.41l-.71-.71A1,1,0,0,0,4.93,6.34Zm12,.29a1,1,0,0,0,.7-.29l.71-.71a1,1,0,1,0-1.41-1.41L17,5.64a1,1,0,0,0,0,1.41A1,1,0,0,0,17.66,7.34ZM21,11H20a1,1,0,0,0,0,2h1a1,1,0,0,0,0-2Zm-9,8a1,1,0,0,0-1,1v1a1,1,0,0,0,2,0V20A1,1,0,0,0,12,19ZM18.36,17A1,1,0,0,0,17,18.36l.71.71a1,1,0,0,0,1.41,0,1,1,0,0,0,0-1.41ZM12,6.5A5.5,5.5,0,1,0,17.5,12,5.51,5.51,0,0,0,12,6.5Zm0,9A3.5,3.5,0,1,1,15.5,12,3.5,3.5,0,0,1,12,15.5Z" />
+                            </svg>
+
+                            <!-- moon icon -->
+                            <svg class="swap-on h-10 w-10 fill-current" xmlns="http://www.w3.org/2000/svg"
+                                viewBox="0 0 24 24">
+                                <path
+                                    d="M21.64,13a1,1,0,0,0-1.05-.14,8.05,8.05,0,0,1-3.37.73A8.15,8.15,0,0,1,9.08,5.49a8.59,8.59,0,0,1,.25-2A1,1,0,0,0,8,2.36,10.14,10.14,0,1,0,22,14.05,1,1,0,0,0,21.64,13Zm-9.5,6.69A8.14,8.14,0,0,1,7.08,5.22v.27A10.15,10.15,0,0,0,17.22,15.63a9.79,9.79,0,0,0,2.1-.22A8.11,8.11,0,0,1,12.14,19.73Z" />
+                            </svg>
+                        </label>
+                    </li>
+                    <li class="pt-2"><a @click.prevent="logout">Logout</a></li>
+                </ul>
+            </div>
+            <div v-if="user.activeCharacter.value !== undefined" class="block">
+                <div class="text-xl font-bold">{{ user.activeCharacter.value.name }}</div>
+                <HealthBar @click="() => console.log('Health edit')" :health="user.activeCharacter.value.health"
+                    class="w-32" />
+            </div>
+        </div>
+
+        <div v-if="user.activeCharacter.value !== undefined" class="flex w-full justify-end">
+            <PerceptionWidget :perception="user.activeCharacter.value.perception" class="w-12 h-12" />
+            <ArmorClassWidget :armor="user.activeCharacter.value.armor" class="w-12 h-12" />
+        </div>
+    </div>
+</template>
+
+<style scoped></style>
