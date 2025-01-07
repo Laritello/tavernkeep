b4b6666e814dd887e13488423aaca22b1da43cb4
Alex Putin <puellamagi999@gmail.com>
2025-01-07 13:04:48 +0300

    Refactoring of delete character button

--- src/tavernkeep.client/src/pages/CharactersPage.vue
+++ src/tavernkeep.client/src/pages/CharactersPage.vue
@@ -21,7 +21,7 @@ async function deleteCharacter(id: string) {
         message: 'Are you sure you want to delete this character?',
     });
     if (result.action !== 'confirm') return;
-    userAccount.deleteCharacter(id);
+    await userAccount.deleteCharacter(id);
 }
 
 async function setActiveCharacter(characterId: string) {
@@ -51,13 +51,26 @@ const isActiveCharacter = (id: string) => id === currentUser.value?.activeCharac
                     >
                         Set active
                     </button>
-                    <v-btn
-                        size="small"
-                        variant="text"
-                        icon="mdi-delete"
+                    <button
+                        class="btn btn-sm btn-square btn-outline"
                         :disabled="isActiveCharacter(character.id)"
                         @click="deleteCharacter(character.id)"
-                    />
+                    >
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            class="h-6 w-6"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            stroke="currentColor"
+                        >
+                            <path
+                                stroke-linecap="round"
+                                stroke-linejoin="round"
+                                stroke-width="2"
+                                d="M6 18L18 6M6 6l12 12"
+                            />
+                        </svg>
+                    </button>
                 </div>
             </div>
         </div>
