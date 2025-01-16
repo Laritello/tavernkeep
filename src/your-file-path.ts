b92f35a30230d3d19cbb2db1779f0195e4f3d787
Alex Putin <puellamagi999@gmail.com>
2025-01-16 21:21:52 +0300

    Delete trash file

--- src/your-file-path.ts
+++
@@ -1,6 +0,0 @@
-const generateArray = (start: number, end: number): number[] => {
-    return Array.from({ length: end - start + 1 }, (_, i) => start + i);
-};
-
-const numbersArray = generateArray(-100, 100);
-console.log(numbersArray);
