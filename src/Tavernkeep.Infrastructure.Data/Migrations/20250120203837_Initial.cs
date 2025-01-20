7a3464952fbdc9d67f8c3ec9ad87919862e10481
Laritello <larik97@gmail.com>
2025-01-20 23:38:59 +0300

    Updated migrations

---
+++ src/Tavernkeep.Infrastructure.Data/Migrations/20250120203837_Initial.cs
@@ -0,0 +1,285 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Tavernkeep.Infrastructure.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Conditions",
+                columns: table => new
+                {
+                    Name = table.Column<string>(type: "TEXT", nullable: false),
+                    Description = table.Column<string>(type: "TEXT", nullable: false),
+                    HasLevels = table.Column<bool>(type: "INTEGER", nullable: false),
+                    Level = table.Column<int>(type: "INTEGER", nullable: false),
+                    Id = table.Column<string>(type: "TEXT", nullable: true),
+                    Modifiers = table.Column<string>(type: "TEXT", nullable: true),
+                    Related = table.Column<string>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Conditions", x => x.Name);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "RefreshTokens",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "TEXT", nullable: false),
+                    UserId = table.Column<Guid>(type: "TEXT", nullable: false),
+                    Token = table.Column<string>(type: "TEXT", nullable: false),
+                    Expires = table.Column<DateTime>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_RefreshTokens", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Character",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "TEXT", nullable: false),
+                    OwnerId = table.Column<Guid>(type: "TEXT", nullable: false),
+                    Name = table.Column<string>(type: "TEXT", nullable: false),
+                    Class = table.Column<string>(type: "TEXT", nullable: false),
+                    Ancestry = table.Column<string>(type: "TEXT", nullable: false),
+                    HeroPoints = table.Column<int>(type: "INTEGER", nullable: false),
+                    Level = table.Column<int>(type: "INTEGER", nullable: false),
+                    Armor = table.Column<string>(type: "TEXT", nullable: false),
+                    Burrow = table.Column<string>(type: "TEXT", nullable: false),
+                    Climb = table.Column<string>(type: "TEXT", nullable: false),
+                    Conditions = table.Column<string>(type: "TEXT", nullable: true),
+                    Fly = table.Column<string>(type: "TEXT", nullable: false),
+                    Health = table.Column<string>(type: "TEXT", nullable: false),
+                    Swim = table.Column<string>(type: "TEXT", nullable: false),
+                    Walk = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Character", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "CharacterAbility",
+                columns: table => new
+                {
+                    Name = table.Column<string>(type: "TEXT", nullable: false),
+                    OwnerId = table.Column<Guid>(type: "TEXT", nullable: false),
+                    Score = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CharacterAbility", x => x.Name);
+                    table.ForeignKey(
+                        name: "FK_CharacterAbility_Character_OwnerId",
+                        column: x => x.OwnerId,
+                        principalTable: "Character",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "TEXT", nullable: false),
+                    Login = table.Column<string>(type: "TEXT", nullable: false),
+                    Password = table.Column<string>(type: "TEXT", nullable: false),
+                    Role = table.Column<string>(type: "TEXT", nullable: false),
+                    ActiveCharacterId = table.Column<Guid>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Users_Character_ActiveCharacterId",
+                        column: x => x.ActiveCharacterId,
+                        principalTable: "Character",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "CharacterSavingThrow",
+                columns: table => new
+                {
+                    Name = table.Column<string>(type: "TEXT", nullable: false),
+                    OwnerId = table.Column<Guid>(type: "TEXT", nullable: false),
+                    AbilityName = table.Column<string>(type: "TEXT", nullable: true),
+                    Proficiency = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CharacterSavingThrow", x => x.Name);
+                    table.ForeignKey(
+                        name: "FK_CharacterSavingThrow_CharacterAbility_AbilityName",
+                        column: x => x.AbilityName,
+                        principalTable: "CharacterAbility",
+                        principalColumn: "Name");
+                    table.ForeignKey(
+                        name: "FK_CharacterSavingThrow_Character_OwnerId",
+                        column: x => x.OwnerId,
+                        principalTable: "Character",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "CharacterSkill",
+                columns: table => new
+                {
+                    Name = table.Column<string>(type: "TEXT", nullable: false),
+                    OwnerId = table.Column<Guid>(type: "TEXT", nullable: false),
+                    Type = table.Column<int>(type: "INTEGER", nullable: false),
+                    AbilityName = table.Column<string>(type: "TEXT", nullable: true),
+                    Proficiency = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CharacterSkill", x => x.Name);
+                    table.ForeignKey(
+                        name: "FK_CharacterSkill_CharacterAbility_AbilityName",
+                        column: x => x.AbilityName,
+                        principalTable: "CharacterAbility",
+                        principalColumn: "Name");
+                    table.ForeignKey(
+                        name: "FK_CharacterSkill_Character_OwnerId",
+                        column: x => x.OwnerId,
+                        principalTable: "Character",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Messages",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "TEXT", nullable: false),
+                    DisplayName = table.Column<string>(type: "TEXT", nullable: false),
+                    SenderId = table.Column<Guid>(type: "TEXT", nullable: false),
+                    Created = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    Discriminator = table.Column<string>(type: "TEXT", maxLength: 34, nullable: false),
+                    RollType = table.Column<string>(type: "TEXT", nullable: true),
+                    Expression = table.Column<string>(type: "TEXT", nullable: true),
+                    Text = table.Column<string>(type: "TEXT", nullable: true),
+                    RecipientId = table.Column<Guid>(type: "TEXT", nullable: true),
+                    Result = table.Column<string>(type: "TEXT", nullable: true),
+                    SavingThrow = table.Column<string>(type: "TEXT", nullable: true),
+                    Skill = table.Column<string>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Messages", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Messages_Users_RecipientId",
+                        column: x => x.RecipientId,
+                        principalTable: "Users",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_Messages_Users_SenderId",
+                        column: x => x.SenderId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Character_OwnerId",
+                table: "Character",
+                column: "OwnerId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CharacterAbility_OwnerId",
+                table: "CharacterAbility",
+                column: "OwnerId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CharacterSavingThrow_AbilityName",
+                table: "CharacterSavingThrow",
+                column: "AbilityName");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CharacterSavingThrow_OwnerId",
+                table: "CharacterSavingThrow",
+                column: "OwnerId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CharacterSkill_AbilityName",
+                table: "CharacterSkill",
+                column: "AbilityName");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CharacterSkill_OwnerId",
+                table: "CharacterSkill",
+                column: "OwnerId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Messages_RecipientId",
+                table: "Messages",
+                column: "RecipientId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Messages_SenderId",
+                table: "Messages",
+                column: "SenderId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_ActiveCharacterId",
+                table: "Users",
+                column: "ActiveCharacterId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_Login",
+                table: "Users",
+                column: "Login",
+                unique: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Character_Users_OwnerId",
+                table: "Character",
+                column: "OwnerId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Character_Users_OwnerId",
+                table: "Character");
+
+            migrationBuilder.DropTable(
+                name: "CharacterSavingThrow");
+
+            migrationBuilder.DropTable(
+                name: "CharacterSkill");
+
+            migrationBuilder.DropTable(
+                name: "Conditions");
+
+            migrationBuilder.DropTable(
+                name: "Messages");
+
+            migrationBuilder.DropTable(
+                name: "RefreshTokens");
+
+            migrationBuilder.DropTable(
+                name: "CharacterAbility");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+
+            migrationBuilder.DropTable(
+                name: "Character");
+        }
+    }
+}
