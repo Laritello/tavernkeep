2fa8316edfb08dfa556bd891d0342b451e75adec
Laritello <larik97@gmail.com>
2025-01-21 13:03:58 +0300

    API: added request for custom skill editing

---
+++ src/Tavernkeep.Application/UseCases/Custom/Commands/EditCustomSkill/EditCustomSkillCommand.cs
@@ -0,0 +1,12 @@
+﻿using MediatR;
+
+namespace Tavernkeep.Application.UseCases.Custom.Commands.EditCustomSkill
+{
+	public class EditCustomSkillCommand(Guid initiatorId, Guid characterId, string oldName, string newName) : IRequest
+	{
+		public Guid InitiatorId { get; set; } = initiatorId;
+		public Guid CharacterId { get; set; } = characterId;
+		public string OldName { get; set; } = oldName;
+		public string NewName { get; set; } = newName;
+	}
+}
---
+++ src/Tavernkeep.Application/UseCases/Custom/Commands/EditCustomSkill/EditCustomSkillCommandHandler.cs
@@ -0,0 +1,47 @@
+﻿using MediatR;
+using Tavernkeep.Application.Interfaces;
+using Tavernkeep.Core.Contracts.Enums;
+using Tavernkeep.Core.Entities.Pathfinder.Properties;
+using Tavernkeep.Core.Exceptions;
+using Tavernkeep.Core.Repositories;
+
+namespace Tavernkeep.Application.UseCases.Custom.Commands.EditCustomSkill
+{
+	public class EditCustomSkillCommandHandler(
+		IUserRepository userRepository,
+		ICharacterRepository characterRepository,
+		INotificationService notificationService
+		) : IRequestHandler<EditCustomSkillCommand>
+	{
+		public async Task Handle(EditCustomSkillCommand request, CancellationToken cancellationToken)
+		{
+			var initiator = await userRepository.FindAsync(request.InitiatorId, cancellationToken: cancellationToken)
+				?? throw new BusinessLogicException("User with specified ID doesn't exist.");
+
+			var character = await characterRepository.GetFullCharacterAsync(request.CharacterId, cancellationToken)
+				?? throw new BusinessLogicException("Character with specified ID doesn't exist.");
+
+			if (character.Owner.Id != request.InitiatorId && initiator.Role != UserRole.Master)
+				throw new InsufficientPermissionException("You do not have the necessary permissions to perform this operation.");
+
+			var skill = character.Skills.FirstOrDefault(s => s.Name == request.OldName)
+				?? throw new BusinessLogicException("Character does not have a skill with this name.");
+
+			if (skill.Type != SkillType.Custom && skill.Type != SkillType.Lore)
+				throw new BusinessLogicException("Edited skill must either have custom or lore type.");
+
+			Skill updated = new(request.NewName, skill.Proficiency, skill.Type)
+			{
+				Owner = skill.Owner,
+				Ability = skill.Ability,
+			};
+
+			character.Skills.Remove(skill);
+			character.Skills.Add(updated);
+
+			characterRepository.Save(character);
+			await characterRepository.CommitAsync(cancellationToken);
+			await notificationService.QueueCharacterNotificationAsync(character, cancellationToken);
+		}
+	}
+}
