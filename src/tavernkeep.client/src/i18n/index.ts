09a22e46d7cbc8c2429ae697391e5e25987a9595
Alex Putin <2861110+AlexPutin@users.noreply.github.com>
2025-02-03 20:47:45 +0300

    Refactor `index.ts` to use I18nOptions type and improve configuration structure.

--- src/tavernkeep.client/src/i18n/index.ts
+++ src/tavernkeep.client/src/i18n/index.ts
@@ -1,17 +1,21 @@
-﻿import { createI18n } from 'vue-i18n';
+﻿import { createI18n, type I18nOptions } from 'vue-i18n';
 
 import en from './locales/en.json';
 import ru from './locales/ru.json';
 
 export type LocaleType = 'en' | 'ru';
 
-export const i18n = createI18n({
-    locale: import.meta.env.VITE_DEFAULT_LOCALE as LocaleType,
-    fallbackLocale: import.meta.env.VITE_FALLBACK_LOCALE as LocaleType,
+const options: I18nOptions = {
     legacy: false,
     globalInjection: true,
+
+    locale: import.meta.env.VITE_DEFAULT_LOCALE,
+    fallbackLocale: import.meta.env.VITE_FALLBACK_LOCALE,
+
     messages: {
         en,
         ru,
     },
-});
+};
+
+export const i18n = createI18n<false, typeof options>(options);
