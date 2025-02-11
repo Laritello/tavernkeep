6de3d4643440b00bdb5be715e995c575801e65a6
Laritello <larik97@gmail.com>
2025-02-11 17:53:38 +0300

    Added encounters controller

--- src/Tavernkeep.Application/UseCases/Encounters/Commands/CreateEncounter/CreateEncounterCommand.cs
+++ src/Tavernkeep.Application/UseCases/Encounters/Commands/CreateEncounter/CreateEncounterCommand.cs
@@ -1,9 +1,11 @@
 ﻿using MediatR;
+using Tavernkeep.Core.Entities.Encounters;
 
 namespace Tavernkeep.Application.UseCases.Encounters.Commands.CreateEncounter
 {
-	public class CreateEncounterCommand(Guid initiatorId) : IRequest
+	public class CreateEncounterCommand(Guid initiatorId, string name) : IRequest<Encounter>
 	{
 		public Guid InitiatorId { get; set; } = initiatorId;
+		public string Name { get; set; } = name;
 	}
 }
---
+++ src/Tavernkeep.Server/Controllers/EncountersController.cs
@@ -0,0 +1,35 @@
+﻿using AutoMapper;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Tavernkeep.Application.UseCases.Encounters.Commands.CreateEncounter;
+using Tavernkeep.Core.Contracts.Encounters.Dtos;
+using Tavernkeep.Core.Contracts.Enums;
+using Tavernkeep.Server.Extensions;
+using Tavernkeep.Server.Middleware;
+
+namespace Tavernkeep.Server.Controllers
+{
+	/// <summary>
+	/// The <see cref="CharactersController"/> class handles character operations within the application.
+	/// </summary>
+	/// <param name="mediator">The <see cref="IMediator"/> instance.</param>
+	/// <param name="mapper">The <see cref="IMapper"/> instance.</param>
+	[ApiController]
+	[Route("/api/[controller]")]
+	public class EncountersController(IMediator mediator, IMapper mapper) : ControllerBase
+	{
+		/// <summary>
+		/// Get all characters.
+		/// </summary>
+		/// <returns>List containing all characters.</returns>
+		[Authorize]
+		[RequiresRole(UserRole.Master)]
+		[HttpPost]
+		public async Task<EncounterDto> CreateEncounterAsync(string name)
+		{
+			var encounter = await mediator.Send(new CreateEncounterCommand(HttpContext.GetUserId(), name));
+			return mapper.Map<EncounterDto>(encounter);
+		}
+	}
+}
