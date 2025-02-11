d840fe8f7e60e8c2a8ff8172c11aca68032d2a28
Laritello <larik97@gmail.com>
2025-02-11 18:49:07 +0300

    Added specification for full encounter

---
+++ src/Tavernkeep.Core/Specifications/Encounters/EncounterFullSpecification.cs
@@ -0,0 +1,12 @@
+﻿using Tavernkeep.Core.Entities.Encounters;
+
+namespace Tavernkeep.Core.Specifications.Encounters
+{
+	public class EncounterFullSpecification : Specification<Encounter>
+	{
+		public EncounterFullSpecification(Guid id) : base(x => x.Id == id)
+		{
+			AddInclude(x => x.Participants);
+		}
+	}
+}
