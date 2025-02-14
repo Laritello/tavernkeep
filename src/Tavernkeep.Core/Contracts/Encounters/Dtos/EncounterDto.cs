84b7369e8721db6bf1a8bbd445ff95d458435102
Laritello <larik97@gmail.com>
2025-02-14 16:48:56 +0300

    Added Status field to the encounter dto

--- src/Tavernkeep.Core/Contracts/Encounters/Dtos/EncounterDto.cs
+++ src/Tavernkeep.Core/Contracts/Encounters/Dtos/EncounterDto.cs
@@ -1,9 +1,12 @@
-﻿namespace Tavernkeep.Core.Contracts.Encounters.Dtos
+﻿using Tavernkeep.Core.Contracts.Enums;
+
+namespace Tavernkeep.Core.Contracts.Encounters.Dtos
 {
 	public class EncounterDto
 	{
 		public Guid Id { get; set; }
 		public required string Name { get; set; }
+		public EncounterStatus Status { get; set; }
 		public required ICollection<EncounterParticipantDto> Participants { get; set; }
 	}
 }
