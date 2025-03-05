02b52c1b745916cc4fc2d9dadbbb7e6497a1c200
Laritello <larik97@gmail.com>
2025-03-05 19:30:19 +0300

    Breaking: new creature format

---
+++ src/Tavernkeep.Core/Entities/Library/Creatures/IStatblock.cs
@@ -0,0 +1,11 @@
+﻿using Tavernkeep.Core.Entities.Pathfinder.Conditions;
+
+namespace Tavernkeep.Core.Entities.Library.Creatures
+{
+	public interface IStatblock
+	{
+		public void ApplyConditions(ICollection<CreatureConditionRecord> conditions);
+		public bool IsDividerEnabled { get; set; }
+		public string HTML { get; }
+	}
+}
