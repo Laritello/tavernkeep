915f841314c5c5c24a9ba0f2a8e6920d292266b7
Laritello <larik97@gmail.com>
2025-03-09 23:45:27 +0300

    API: creature token implementations

---
+++ src/Tavernkeep.Core/Entities/Library/Creatures/Tokens/KeywordToken.cs
@@ -0,0 +1,32 @@
+﻿using Tavernkeep.Core.Entities.Library.Creatures.Abstractions.Tokens;
+using Tavernkeep.Core.Extensions;
+
+namespace Tavernkeep.Core.Entities.Library.Creatures.Tokens
+{
+	public class KeywordToken : IKeywordToken
+	{
+		public required string Name { get; set; }
+		public string Html
+		{
+			get
+			{
+				return $"{Name.HtmlWrapIn("strong")}";
+			}
+		}
+
+		public void Attach(IToken token)
+		{
+			if (token is not IKeywordToken keywordToken)
+			{
+				throw new ArgumentException("Token format is not supported", nameof(token));
+			}
+
+			Name += $" {keywordToken.Name}";
+		}
+
+		public bool IsAttachable(IToken token)
+		{
+			return token is IKeywordToken;
+		}
+	}
+}
---
+++ src/Tavernkeep.Core/Extensions/StringExtensions.cs
@@ -0,0 +1,7 @@
+﻿namespace Tavernkeep.Core.Extensions
+{
+	public static class StringExtensions
+	{
+		public static string HtmlWrapIn(this string text, string container) => $"<{container}>{text}</{container}>";
+	}
+}
