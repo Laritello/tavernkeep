f4e7787f8408baf30d85eaa69d4c60a74c9f5724
Laritello <larik97@gmail.com>
2025-03-09 23:38:39 +0300

    API: creature token abstractions

---
+++ src/Tavernkeep.Core/Entities/Library/Creatures/Abstractions/Tokens/ITextToken.cs
@@ -0,0 +1,9 @@
+﻿namespace Tavernkeep.Core.Entities.Library.Creatures.Abstractions.Tokens
+{
+	public interface ITextToken : IToken, IAttachableToken
+	{
+		public string Text { get; set; }
+		public bool IsBold { get; set; }
+		public bool IsItalic { get; set; }
+	}
+}
