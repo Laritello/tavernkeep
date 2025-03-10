581aaa26b208d6643e5c98bfb0ffd9e85076ae89
Laritello <larik97@gmail.com>
2025-03-10 23:07:19 +0300

    New token types added

---
+++ src/Tavernkeep.Core/Entities/Library/Creatures/Tokens/ListItemToken.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tavernkeep.Core.Entities.Library.Creatures.Abstractions.Tokens;
+
+namespace Tavernkeep.Core.Entities.Library.Creatures.Tokens
+{
+	public class ListItemToken : IListItemToken
+	{
+		public bool IsStart { get; set; }
+
+		public string Html => IsStart ? "<li>" : "</li>";
+	}
+}
