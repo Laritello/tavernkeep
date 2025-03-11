52380982b2d3f400906921ff593bac19fdb0467d
Laritello <larik97@gmail.com>
2025-03-11 22:22:16 +0300

    Fixed URLs in ActionToken

--- src/Tavernkeep.Core/Statblocks/Tokens/ActionToken.cs
+++ src/Tavernkeep.Core/Statblocks/Tokens/ActionToken.cs
@@ -13,11 +13,11 @@ namespace Tavernkeep.Core.Statblocks.Tokens
 			{
 				return Amount switch
 				{
-					ActionAmount.Free => "<img src='./resources/pf-action-free.png' class='text-img'></img>",
-					ActionAmount.Reaction => "<img src='./resources/pf-action-reaction.png' class='text-img'></img>",
-					ActionAmount.One => "<img src='./resources/pf-action-1.png' class='text-img'></img>",
-					ActionAmount.Two => "<img src='./resources/pf-action-2.png' class='text-img'></img>",
-					ActionAmount.Three => "<img src='./resources/pf-action-3.png' class='text-img'></img>",
+					ActionAmount.Free => "<img src='/assets/images/pf-action-free.png' class='text-img'></img>",
+					ActionAmount.Reaction => "<img src='/assets/images/pf-action-reaction.png' class='text-img'></img>",
+					ActionAmount.One => "<img src='/assets/images/pf-action-1.png' class='text-img'></img>",
+					ActionAmount.Two => "<img src='/assets/images/pf-action-2.png' class='text-img'></img>",
+					ActionAmount.Three => "<img src='/assets/images/pf-action-3.png' class='text-img'></img>",
 					_ => throw new NotImplementedException()
 				};
 			}
