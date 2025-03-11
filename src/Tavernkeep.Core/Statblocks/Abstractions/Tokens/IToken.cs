1a6c10857657aaad750bc8f16735e88031f9c59d
Laritello <larik97@gmail.com>
2025-03-11 22:08:00 +0300

    Add JSON polymorpism support for IToken

--- src/Tavernkeep.Core/Statblocks/Abstractions/Tokens/IToken.cs
+++ src/Tavernkeep.Core/Statblocks/Abstractions/Tokens/IToken.cs
@@ -1,11 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Text.Json.Serialization;
+using Tavernkeep.Core.Statblocks.Tokens;
 
 namespace Tavernkeep.Core.Statblocks.Abstractions.Tokens
 {
+	[JsonDerivedType(typeof(ActionToken), nameof(ActionToken))]
+	[JsonDerivedType(typeof(TextToken), nameof(TextToken))]
+	[JsonDerivedType(typeof(KeywordToken), nameof(KeywordToken))]
+	[JsonDerivedType(typeof(ListToken), nameof(ListToken))]
+	[JsonDerivedType(typeof(ListItemToken), nameof(ListItemToken))]
 	public interface IToken
 	{
 		public string Html { get; }
