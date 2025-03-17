434679c630c1f5bbdcf525d6f809d23c870f4471
Laritello <larik97@gmail.com>
2025-03-17 22:34:39 +0300

    New token implementations

---
+++ src/Tavernkeep.Core/Statblocks/Tokens/BonusToken.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tavernkeep.Core.Statblocks.Abstractions.Tokens;
+
+namespace Tavernkeep.Core.Statblocks.Tokens
+{
+	public class BonusToken(int bonus) : IBonusToken
+	{
+		public int Bonus { get; set; } = bonus;
+		public string Html => $"{(Bonus >= 0 ? "+" : "")}{Bonus}";
+
+		public IToken Copy()
+		{
+			return new BonusToken(Bonus);
+		}
+	}
+}
---
+++ src/Tavernkeep.Core/Statblocks/Tokens/SkillToken.cs
@@ -0,0 +1,17 @@
+﻿using Tavernkeep.Core.Statblocks.Abstractions.Tokens;
+
+namespace Tavernkeep.Core.Statblocks.Tokens
+{
+	public class SkillToken(string name, int bonus) : ISkillToken
+	{
+		public string Name { get; set; } = name;
+		public int Bonus { get; set; } = bonus;
+
+		public string Html => $"{Name} {(Bonus >= 0 ? "+" : "")}{Bonus}";
+
+		public IToken Copy()
+		{
+			return new SkillToken(Name, Bonus);
+		}
+	}
+}
