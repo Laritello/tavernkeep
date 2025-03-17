c21443c704026b298d332460c8977710c22cbe0a
Laritello <larik97@gmail.com>
2025-03-17 22:32:38 +0300

    New token types

---
+++ src/Tavernkeep.Core/Statblocks/Abstractions/Tokens/IBonusToken.cs
@@ -0,0 +1,7 @@
+﻿namespace Tavernkeep.Core.Statblocks.Abstractions.Tokens
+{
+	public interface IBonusToken : IToken
+	{
+		public int Bonus { get; set; }
+	}
+}
---
+++ src/Tavernkeep.Core/Statblocks/Abstractions/Tokens/ISkillToken.cs
@@ -0,0 +1,8 @@
+﻿namespace Tavernkeep.Core.Statblocks.Abstractions.Tokens
+{
+	public interface ISkillToken : IToken
+	{
+		public string Name { get; set; }
+		public int Bonus { get; set; }
+	}
+}
