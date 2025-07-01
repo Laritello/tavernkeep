e176aec5386fc96551059f32641c59ba6ee26ec2
Laritello <larik97@gmail.com>
2025-07-01 23:05:04 +0300

    Removed parser

--- src/Tavernkeep.Statblock.Parser/Parsing/Abstractions/ITokenizer.cs
+++
@@ -1,9 +0,0 @@
-﻿using Tavernkeep.Core.Statblocks.Abstractions.Tokens;
-
-namespace Tavernkeep.Statblock.Parser.Parsing.Abstractions
-{
-	public interface ITokenizer<T>
-	{
-		public List<IToken> Tokenize(T source);
-	}
-}
